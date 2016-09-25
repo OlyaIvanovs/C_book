0bd7335910b80fda82088e1b45eb724ea18c54ad
Olya Ivanova <olya@ivanovs.info>
2016-09-25 20:15:38 +0500

    Ex Chapter5 Pointers.

--- chapter5/ex5_7.c
+++ chapter5/ex5_7.c
@@ -5,63 +5,9 @@
 #define MAXLINES 5000
 #define MAXLEN 1000 /* max length of any input line */
 
-char *lineptr[MAXLINES]; /* pointers to text lines */
+char *lineptr[MAXLINES]; /* pointers to text lines */\
 char *alloc(int);
 
-int readlines(char *lineptr[], int nlines);
-int getline1(char *, int);
-void writelines(char *lineptr[], int nlines);
-void my_qsort(char *lineptr[], int left, int right);
-
-int main() {
-  int nlines;
-
-  if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
-    my_qsort(lineptr, 0, nlines - 1);
-    writelines(lineptr, nlines);
-    return 0;
-  } else {
-    printf("error: input too big to sort\n");
-    return 1;
-  }
-}
-
-int readlines(char *lineptr[], int maxlines) {
-  int len, nlines;
-  char *p, line[MAXLEN];
-
-  nlines = 0;
-  while((len = getline1(line, MAXLEN)) > 0) {
-    if (nlines >= maxlines || (p = malloc(len)) == NULL) {
-      return -1;
-    } else {
-      line[len-1] = '\0';
-      strcpy(p, line);
-      lineptr[nlines++] = p;
-    }
-  }
-  return nlines;
-}
-
-int readlines2() {
-  int len, nlines;
-  char *p, line[MAXLEN];
-
-  nlines = 0;
-  while((len = getline1(line, MAXLEN)) > 0) {
-
-  }
-  return nlines;
-}
-
-
-void writelines(char *lineptr[], int nlines) {
-  int i;
-
-  while (nlines-- > 0) {
-    printf("%s\n", *lineptr++);
-  }
-}
 
 int getline1(char s[], int lim) {
   int c, i;
@@ -77,32 +23,41 @@ int getline1(char s[], int lim) {
   return i;
 }
 
-void my_qsort(char *v[], int left, int right) {
-  int i, last;
-  void swap(char *v[], int i, int j);
+void writelines(char lines[][MAXLEN], int nlines) {
+  int i;
 
-  if (left >= right) {
-    return;
+  while (nlines-- > 0) {
+    printf("%s", lines[nlines]);
   }
-  swap(v, left, (left+right)/2);
-  last = left;
-  for (i = left + 1; i <= right; i++) {
-    if (strcmp(v[i], v[left]) < 0) {
-      swap(v, ++last, i);
+}
+
+int readlines2(char lines[][MAXLEN], int maxlines) {
+  int len, nlines;
+
+  nlines = 0;
+  while((len = getline1(lines[nlines], MAXLEN)) > 0) {
+    if (nlines >= maxlines) {
+      return -1;
+    } else {
+      lines[nlines++][len] = '\0';
     }
   }
-  swap(v, left, last);
-  my_qsort(v, left, last-1);
-  my_qsort(v, last+1, right);
+  return nlines;
 }
 
-void swap(char *v[], int i, int j) {
-  char *temp;
 
-  temp = v[i];
-  v[i] = v[j];
-  v[j] = temp;
+int main() {
+  int nlines;
+  char lines[MAXLINES][MAXLEN];
+
+  // if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
+  if ((nlines = readlines2(lines, MAXLINES)) >= 0) {
+    writelines(lines, nlines);
+    return 0;
+  } else {
+    printf("error: input too big to sort\n");
+    return 1;
+  }
 }
-
 
 
