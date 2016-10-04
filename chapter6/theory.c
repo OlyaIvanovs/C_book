f4d3fcea8aa1b122000b07598aad5703a6370a5b
Olya Ivanova <olya@ivanovs.info>
2016-10-04 11:38:29 +0500

    Ex Chapter6

--- chapter6/theory.c
+++ chapter6/theory.c
@@ -1,49 +1,116 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
 
-#define min(a, b) ((a) < (b) ? (a) : (b));
-#define max(a, b) ((a) > (b) ? (a) : (b))
+#define MAXWORD 100
+#define BUFSIZE 100
 
-struct point {
-  int x;
-  int y;
+char buf[BUFSIZE];
+int bufp = 0;
+
+struct tnode {
+  char *word;
+  int count;
+  struct tnode *left;
+  struct tnode *right;
 };
 
-struct rect {
-  struct point pt1;
-  struct point pt2;
-};
+struct tnode *addtree(struct tnode *, char *);
+struct tnode *talloc(void);
+void treeprint(struct tnode *);
+int getword(char *, int);
+char *my_strdup(char *s);
 
-struct point makepoint(int x, int y) {
-  struct point temp;
-  temp.x = x;
-  temp.y = y;
-  return temp;
+
+/* word frequency count */
+int main() {
+  struct tnode *root;
+  char word[MAXWORD];
+
+  root = NULL;
+  while (getword(word, MAXWORD) != EOF) {
+    if (isalpha(word[0])) {
+      root = addtree(root, word);
+    }
+  }
+  treeprint(root);
+  return 0;
 }
 
-struct rect screen;
-struct point middle;
+struct tnode *addtree(struct tnode *p, char *w) {
+  int cond;
 
-screen.pt1 = makepoint(0, 0);
-screen.pt2 = makepoint(XMAX, YMAX);
-middle = makepoint((screen.pt1.x + screen.pt2.x)/2, (screen.pt1.y + screen.pt2.y)/2);
-
-
-struct addpoint(struct point p1, struct point p2) {
-  p1.x += p2.x;
-  p1.y += p2.y;
-  return p1;
+  if (p == NULL) {
+    p = talloc(); /* make a new node */
+    p->word = my_strdup(w);
+    p->count = 1;
+    p->left = p->right = NULL;
+  } else if ((cond = strcmp(w, p->word)) == 0) {
+    p->count++;
+  } else if (cond < 0) {
+    p->left = addtree(p->left, w);
+  } else {
+    p->right = addtree(p->right, w);
+  }
+  return p;
 }
 
 
-int ptinrect(struct point p, struct rect r) {
-  return p.x >= r.pt1.x && p.x < r.pt2.x && p.y >= r.pt1.y && p.y < r.pt2.y;
+void treeprint(struct tnode *p) {
+  if (p != NULL) {
+    treeprint(p->left);
+    printf("%4d %s\n", p->count, p->word);
+    treeprint(p->right);
+  }
 }
 
-struct rect cannonrect(struct rect r) {
-  struct rect temp;
+struct tnode *talloc(void) {
+  return  (struct tnode *) malloc(sizeof(struct tnode));
+}
 
-  temp.pt1.x = min(r.pt1.x, r.pt2.x);
-  temp.pt1.y = min(r.pt1.y, r.pt2.y);
-  temp.pt2.x = max(r.pt1.x, r.pt2.x);
-  temp.pt2.y = max(r.pt1.y, r.pt2.y);
+char *my_strdup(char *s) {
+  char *p;
+
+  p = (char *)malloc(strlen(s) + 1);
+  if (p != NULL)
+    strcpy(p, s);
+  return p;
+}
+
+int getch(void) {
+  return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c) {
+  if (bufp >= BUFSIZE) {
+    printf("ungetch: too many characters\n");
+  } else {
+    buf[bufp++] = c;
+  }
+}
+
+int getword(char *word, int lim) {
+  int c, getch(void);
+  void ungetch(int);
+  char *w = word;
+
+  while(isspace(c = getch())) 
+    ;
+
+  if (c != EOF) {
+    *w++ = c;
+  }
+  if (!isalpha(c)) {
+    *w = '\n';
+    return c;
+  }
+  for (; --lim > 0; w++) {
+    if (!isalnum(*w = getch())) {
+      ungetch(*w);
+      break;
+    }
+  }
+  *w = '\0';
+  return word[0];
 }
