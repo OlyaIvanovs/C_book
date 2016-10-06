e722384c8a2e57703ccdc48a39eeaf24df493ef9
Olya Ivanova <olya@ivanovs.info>
2016-10-06 10:45:57 +0500

    Ex chapter6

--- chapter6/ex6_2.c
+++ chapter6/ex6_2.c
@@ -17,11 +17,22 @@ struct tnode {
   struct tnode *right;
 };
 
+struct tnode_word {
+  char *root;
+  int count;
+  struct tnode_word *left;
+  struct tnode_word *right;
+};
+
 struct tnode *addtree_root(struct tnode *, char *, int n);
-struct tnode *addtree(struct tnode *, char *);
 struct tnode *talloc(void);
+void addsubtree(struct tnode *p);
 void treeprint(struct tnode *);
-void dublicateprint(struct tnode *);
+
+struct tnode_word *addtree(struct tnode_word *, char *);
+struct tnode_word *walloc(void);
+void subtreeprint(struct tnode_word *);
+
 int getword(char *, int);
 char *my_strdup(char *s, int n);
 char *my_strdup2(char *s);
@@ -37,10 +48,11 @@ int main() {
   root = NULL;
   while (getword(word, MAXWORD) != EOF) {
     if (isalpha(word[0])) {
-      root = addtree(root, word, first);
+      root = addtree_root(root, word, first);
     }
   }
-  treeprint(root);
+  printf("\n\nDUBLICATE LISTS:\n");
+  addsubtree(root);
   return 0;
 }
 
@@ -59,32 +71,67 @@ struct tnode *addtree_root(struct tnode *p, char *w, int n) {
     p->words[k] = my_strdup2(w);
     p->count++;
   } else if (cond < 0) {
-    p->left = addtree(p->left, w, n);
+    p->left = addtree_root(p->left, w, n);
   } else {
-    p->right = addtree(p->right, w, n);
+    p->right = addtree_root(p->right, w, n);
   }
   return p;
 }
 
 
-void treeprint(struct tnode *p) {
-  int i;
+struct tnode_word *addtree(struct tnode_word *p, char *w) {
+  int cond;
 
-  if (p != NULL) {
-    treeprint(p->left);
-    if (p->count > 1) {
-      printf("%4d %s\n", p->count, p->root);
-      for (i=0; i < p->count; i++) {
-        printf("%s\n", p->words[i]);
-      }
+  if (p == NULL) {
+    p = walloc(); /* make a new node */
+    p->root = my_strdup2(w);
+    p->count = 1;
+    p->left = p->right = NULL;
+  } else if ((cond = strcmp(w, p->root)) == 0) {
+    p->count++;
+  } else if (cond < 0) {
+    p->left = addtree(p->left, w);
+  } else {
+    p->right = addtree(p->right, w);
+  }
+  return p;
+}
+
+
+void addsubtree(struct tnode *p) {
+  int i;
+  struct tnode_word *root;
+
+  root = NULL;
+  if (p != NULL && p->count > 1) {
+    addsubtree(p->left);
+    for (i = 0; i < p->count; i++)  {
+      root = addtree(root, p->words[i]);
     }
-    treeprint(p->right);
+    printf("\nROOT: %s %d\n", p->root, p->count);
+    subtreeprint(root);
+    addsubtree(p->right);
   }
 }
+
+void subtreeprint(struct tnode_word *p) {
+  if (p != NULL) {
+    subtreeprint(p->left);
+    printf("\t\t%s %d\n", p->root, p->count);
+    subtreeprint(p->right);
+  }
+}
+
 
 struct tnode *talloc(void) {
   return  (struct tnode *) malloc(sizeof(struct tnode));
 }
+
+
+struct tnode_word *walloc(void) {
+  return  (struct tnode_word *) malloc(sizeof(struct tnode_word));
+}
+
 
 char *my_strdup(char *s, int n) {
   char *p;
@@ -95,6 +142,7 @@ char *my_strdup(char *s, int n) {
   return p;
 }
 
+
 char *my_strdup2(char *s) {
   char *p;
 
@@ -104,9 +152,11 @@ char *my_strdup2(char *s) {
   return p;
 }
 
+
 int getch(void) {
   return (bufp > 0) ? buf[--bufp] : getchar();
 }
+
 
 void ungetch(int c) {
   if (bufp >= BUFSIZE) {
@@ -115,6 +165,7 @@ void ungetch(int c) {
     buf[bufp++] = c;
   }
 }
+
 
 int getword(char *word, int lim) {
   int c, getch(void);
@@ -127,6 +178,17 @@ int getword(char *word, int lim) {
   if (c != EOF) {
     *w++ = c;
   }
+
+  //Skip comments
+  if (c == '/' && (c = getch()) == '*') {
+    while ((c = getch()) != '*')
+      ;
+    if ((c = getch())  == '/') {
+      *w = '\n';
+      return c;
+    }
+  }
+
   if (!isalpha(c)) {
     *w = '\n';
     return c;
